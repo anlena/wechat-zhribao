2c7ca0863574e05f1094cfd310970b474a3da50d
anlena <superiron_man@163.com>
2018-03-27 11:51:44 +0800

    基本功能已有

---
+++ app/pages/theme/theme.js
@@ -0,0 +1,20 @@
+// pages/theme/theme.js
+Page({
+  data:{
+    list:[]
+  },
+  onLoad:function(){
+    var that = this;
+    wx.request({
+      url: 'http://news-at.zhihu.com/api/4/themes',
+      header:{
+        'Content-type': 'application / json'
+      },
+      success:function(res){
+        that.setData({
+          list:res.data.others
+        })
+      }
+    })
+  }
+})
